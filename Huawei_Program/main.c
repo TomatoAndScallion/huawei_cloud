379e2c305843dd6c8a6303135ad28e57d97fd249
hope <479874887wen@sina.com>
2016-03-08 22:21:10 +0800

    first day

--- Huawei_Program/main.c
+++ Huawei_Program/main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+void ReadFile(*FILE fl)
+{
+	
+ } 
 int main(int argc, char *argv[]) {
 	
 	return 0;
